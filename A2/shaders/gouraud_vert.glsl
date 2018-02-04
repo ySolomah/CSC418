a7d2f0aed5ede090258fe0c063f7303822294c42
ySolomah <yassir.solomah@gmail.com>
2018-02-04 16:00:44 -0500

    A2 initial commit

---
+++ A2/shaders/gouraud_vert.glsl
@@ -0,0 +1,33 @@
+attribute vec3 position; // Given vertex position in object space
+attribute vec3 normal; // Given vertex normal in object space
+attribute vec3 worldPosition; // Given vertex position in world space
+
+uniform mat4 projection, modelview, normalMat; // Given scene transformation matrices
+uniform vec3 eyePos;	// Given position of the camera/eye/viewer
+
+// These will be given to the fragment shader and interpolated automatically
+varying vec3 normalInterp; // Normal
+varying vec3 vertPos; // Vertex position
+varying vec3 viewVec; // Vector from the eye to the vertex
+varying vec4 color;
+
+uniform float Ka;   // Ambient reflection coefficient
+uniform float Kd;   // Diffuse reflection coefficient
+uniform float Ks;   // Specular reflection coefficient
+uniform float shininessVal; // Shininess
+
+// Material color
+uniform vec3 ambientColor;
+uniform vec3 diffuseColor;
+uniform vec3 specularColor;
+uniform vec3 lightPos; // Light position in camera space
+
+
+void main(){
+  // Your solution should go here.
+  // Only the ambient colour calculations have been provided as an example.
+
+  vec4 vertPos4 = modelview * vec4(position, 1.0);
+  gl_Position = projection * vertPos4;
+  color = vec4(ambientColor, 1.0); 
+}
---
+++ A2/shaders/halftone_frag.glsl
@@ -0,0 +1,27 @@
+precision mediump float; // It is required to set a floating point precision in all fragment shaders.
+
+// Interpolated values from vertex shader
+varying vec3 normalInterp; // Normal
+varying vec3 vertPos; // Vertex position
+varying vec3 viewVec; // Interpolated view vector
+
+// uniform values remain the same across the scene
+uniform float Ka;   // Ambient reflection coefficient
+uniform float Kd;   // Diffuse reflection coefficient
+uniform float Ks;   // Specular reflection coefficient
+uniform float shininessVal; // Shininess
+
+// Material color
+uniform vec3 ambientColor;
+uniform vec3 diffuseColor;
+uniform vec3 specularColor;
+
+uniform vec3 lightPos; // Light position in camera space
+
+// HINT: Use the built-in variable gl_FragCoord to get the screen-space coordinates
+
+void main() {
+  // Your solution should go here.
+  // Only the background color calculations have been provided as an example.
+  gl_FragColor = vec4(diffuseColor, 1.0);
+}
