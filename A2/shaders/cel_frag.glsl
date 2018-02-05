449ef5dd6f8493458149df9e7f1cbdbe75fb2743
Yassir Solomah <yassir.solomah@gmail.com>
2018-02-05 15:57:07 -0500

    cell shade

--- A2/shaders/cel_frag.glsl
+++ A2/shaders/cel_frag.glsl
@@ -18,9 +18,64 @@ uniform vec3 specularColor;
 
 uniform vec3 lightPos; // Light position in camera space
 
+
 void main() {
   // Your solution should go here
   // Only the ambient colour calculations have been provided as an example.
+  // Your solution should go here.
+  // Only the ambient colour calculations have been provided as an example.
+  vec3 normal = normalize(normalInterp);
+  vec3 lightVec = normalize(lightPos - vertPos);
+  vec3 viewVec = normalize(-vertPos);
 
-  gl_FragColor = vec4(ambientColor, 1.0);
+  float lambertCoeff = dot(lightVec, normal);
+  if(lambertCoeff < 0.0) {
+  	lambertCoeff = 0.0;
+  }
+
+  vec3 ambientLight = ambientColor * vec3(Ka);
+  vec3 diffuseLight = diffuseColor * vec3(Kd) * vec3(lambertCoeff);
+
+  vec3 refVec = vec3(2.0) * vec3(dot(lightVec, normal)) * normal - lightVec;
+
+  float rDotV = dot(refVec, viewVec);
+  if(rDotV < 0.0) {
+  	rDotV = 0.0;
+  }
+
+  vec3 specularLight = vec3(Ks) * vec3(pow(rDotV, shininessVal)) * specularColor;
+
+  float specularIntensity = dot(specularLight, normal);
+  if(specularIntensity > 0.85) {
+    specularLight += vec3(2048);
+    specularLight = specularLight / vec3(16);
+    specularLight = specularLight / vec3(255);
+  } else {
+    specularLight = vec3(0.0, 0.0, 0.0);
+  }
+
+  vec4 totalLight = vec4(ambientLight + diffuseLight + specularLight, 1.0);
+  float reduction = 1.0;
+  if(lambertCoeff > 0.95) {
+  	reduction = 1.0;
+  	totalLight = vec4(reduction) * totalLight;
+  } else if(lambertCoeff > 0.7) {
+  	reduction = 0.8;
+  	totalLight = vec4(vec3(reduction), 1.0) * totalLight;
+  } else if(lambertCoeff > 0.5) {
+  	reduction = 0.7;
+  	totalLight = vec4(vec3(reduction), 1.0) * totalLight;
+  } else if(lambertCoeff > 0.2) {
+  	reduction = 0.5;
+  	totalLight = vec4(vec3(reduction), 1.0) * totalLight;
+  } else if(lambertCoeff > 0.1) {
+  	reduction = 0.3;
+  	totalLight = vec4(vec3(reduction), 1.0) * totalLight;
+  } else {
+  	reduction = 0.1;
+  	totalLight = vec4(vec3(reduction), 1.0) * totalLight;
+  }
+
+  //gl_FragColor = vec4(ambientColor, 1.0);
+  gl_FragColor = vec4(totalLight);
 }
--- A2/shaders/cel_vert.glsl
+++ A2/shaders/cel_vert.glsl
@@ -13,5 +13,8 @@ varying vec3 viewVec; // Vector from the eye to the vertex
 void main() {
   // Your solution should go here.
   vec4 vertPos4 = modelview * vec4(position, 1.0);
+  vertPos = vec3(vertPos4);
+  normalInterp = normalize(mat3(normalMat) * normal);
+  viewVec = normalize(eyePos - worldPosition);
   gl_Position = projection * vertPos4;
 }
