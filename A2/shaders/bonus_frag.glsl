8270317b3ab8f6e313d1a4c18b8a2b9f5f39cceb
ySolomah <yassir.solomah@gmail.com>
2018-03-04 22:05:24 -0500

    Update bonus_frag.glsl

--- A2/shaders/bonus_frag.glsl
+++ A2/shaders/bonus_frag.glsl
@@ -24,8 +24,76 @@ uniform vec3 lightPos; // Light position in camera space
 
 uniform sampler2D uSampler;	// 2D sampler for the earth texture
 
+float beckmannDistribution(float x, float roughness) {
+  float NdotH = max(x, 0.0001);
+  float cos2Alpha = NdotH * NdotH;
+  float tan2Alpha = (cos2Alpha - 1.0) / cos2Alpha;
+  float roughness2 = roughness * roughness;
+  float denom = 3.141592653589793 * roughness2 * cos2Alpha * cos2Alpha;
+  return exp(tan2Alpha / roughness2) / denom;
+}
+
+
 void main() {
   // Your solution should go here.
   // Only the ambient colour calculations have been provided as an example.
-  gl_FragColor = vec4(ambientColor, 1.0);
+
+  vec3 normal = normalize(normalInterp);
+  vec3 lightVec = normalize(lightPos - vertPos);
+
+  float lambertCoeff = dot(lightVec, normal);
+  if(lambertCoeff < 0.0) {
+  	lambertCoeff = 0.0;
+  }
+
+  float viewNorm = dot(viewVec, normal);
+  if(viewNorm < 0.0) {
+  	viewNorm = 0.0;
+  }
+
+  vec3 ambientLight = ambientColor * vec3(Ka);
+  vec3 diffuseLight = diffuseColor * vec3(Kd) * vec3(lambertCoeff);
+
+
+  vec3 halfVec = normalize(lightVec + viewVec);
+
+
+  float halfNorm = dot(normal, halfVec);
+  if(halfNorm < 0.0) {
+  	halfNorm = 0.0;
+  }
+
+  float halfView = dot(viewVec, halfVec);
+  if(halfView < 0.001) {
+  	halfView = 0.001;
+  }
+
+  float weight = 2.0 * halfNorm / halfView;
+  float specWeight = 0.0;
+
+  if(lambertCoeff > viewNorm) {
+  	specWeight = viewNorm;
+  } else {
+  	specWeight = lambertCoeff;
+  }
+
+  if(specWeight > 1.0) {
+  	specWeight = 1.0;
+  }
+
+  float beckmannWeight = beckmannDistribution(halfNorm, 0.2);
+
+
+  float fresnelWeight = pow(1.0 - viewNorm, shininessVal);
+
+  float finalScale = 3.14159 * viewNorm * lambertCoeff;
+
+  if(finalScale < 0.000001) {
+  	finalScale = 0.000001;
+  }
+
+
+  float finalSpec = Ks * specWeight * fresnelWeight * beckmannWeight /  finalScale;
+
+  gl_FragColor = vec4(ambientLight + diffuseLight + (vec3(specularColor) * vec3(finalSpec)), 1.0);
 }
